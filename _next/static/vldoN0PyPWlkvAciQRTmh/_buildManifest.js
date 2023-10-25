ca25c8ee3837bc209a659551e062479cd63dd331
CI <CI@example.com>
2023-10-25 08:42:48 +0000

    deploy

---
+++ _next/static/vldoN0PyPWlkvAciQRTmh/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/40bcc10dd94f14af.css","static/chunks/pages/index-58f6eb983df73b45.js"],"/_error":["static/chunks/pages/_error-b646007f40c4f0a8.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
